0d1d2b487f3df7e33288546462f986da01f21cea
Hong La <hongla@kms-technology.com>
2021-06-27 17:50:37 +0700

    Add Prettier and more ESLint rules

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-/* eslint-disable linebreak-style */
-const express = require('express');
-const mongoose = require('mongoose');
-const TaskModel = require('./models/task');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const TaskModel = require('./models/task')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 // connect to MongoDB
-mongoose.connect('mongodb+srv://namlnnguyen:1qazQAZ@cluster0.dnem1.mongodb.net/todoApp?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  'mongodb+srv://namlnnguyen:1qazQAZ@cluster0.dnem1.mongodb.net/todoApp?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true
+  }
+)
 
 app.get('/', async (req, res) => {
   // render all current task in database
   const task = new TaskModel({
     taskContent: 'cleaning house',
-    taskCompleted: false,
-  });
+    taskCompleted: false
+  })
 
   try {
-    await task.save();
-    res.send('inserted mock data');
+    await task.save()
+    res.send('inserted mock data')
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
 app.listen(3001, () => {
-  console.log('Server is running at http://localhost:3001');
-});
+  console.log('Server is running at http://localhost:3001')
+})
--- models/task.js
+++ models/task.js
@@ -1,18 +1,15 @@
-/* eslint-disable indent */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable linebreak-style */
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
-    taskContent: {
-        type: 'string',
-        required: true,
-    },
-    taskCompleted: {
-        type: 'boolean',
-        required: true,
-    },
-});
+  taskContent: {
+    type: 'string',
+    required: true
+  },
+  taskCompleted: {
+    type: 'boolean',
+    required: true
+  }
+})
 
-const tasks = mongoose.model('task', taskSchema);
-module.exports = tasks;
+const tasks = mongoose.model('task', taskSchema)
+module.exports = tasks
