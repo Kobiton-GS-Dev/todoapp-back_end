e48d7a89664193a9584d1e5828921c690623a3cc
nhatnam23012000 <nhatnam23012000@gmail.com>
2021-06-29 17:11:11 +0700

    fixed: fixed the problem with the POST method

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,30 +1,41 @@
-const TaskModel = require('../models/task');
+const TaskModel = require('../models/task')
 
 // find all tasks in the database
 module.exports.findAllTasks = async (req, res) => {
-  const tasks = await TaskModel.find();
-  res.json(tasks);
+  const tasks = await TaskModel.find()
+  res.json(tasks)
 }
 
 // add new task to database
 module.exports.addTask = async (req, res) => {
-  const tasks = await TaskModel.create(req.body)
-  res.json(tasks);
+  const title = req.body.title
+  const isCompleted = req.body.isCompleted
+  const createdAt = new Date().toLocaleString()
+  const updatedAt = ''
+  const tasksTobeAdded = new TaskModel({
+    title,
+    isCompleted,
+    createdAt,
+    updatedAt,
+  })
+
+  const tasks = await tasksTobeAdded.save()
+  return res.json(tasks)
 }
 
 // update tasks
 module.exports.updateTask = async (req, res) => {
   await TaskModel.findByIdAndUpdate(req.params.id, req.body, (err, tasks) => {
-    if (err) return res.status(500).json(err);
-    return res.json(tasks);
-  });
+    if (err) return res.status(500).json(err)
+    return res.json(tasks)
+  })
 }
 
 // delete tasks
 module.exports.deleteTask = async (req, res) => {
   await TaskModel.findByIdAndRemove(req.params.id, (err, tasks) => {
-    if (err) return res.status(500).json(err);
+    if (err) return res.status(500).json(err)
     const message = 'Deleted task'
-    return res.json(message);
+    return res.json(message)
   })
 }
